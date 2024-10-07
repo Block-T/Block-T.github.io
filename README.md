b4afba89362d245182a1ff023aecbb2d49eb919e
Block-T <coodbt@outlook.com>
2024-10-07 08:54:17 +0800

    A

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Block-T.github.io
-在线游玩《我的世界Minecraft》，支持JAVA在线网页版和JavaScript在线网页版，并提供其他版本的合集，免费体验MinecraftWeb的所有网页版本。
+-在线游玩《我的世界Minecraft》，支持JAVA在线网页版和JavaScript在线网页版，并提供其他版本的合集，免费体验MinecraftWeb的所有网页版本。
+
+# 作者信息:
+-邮箱: coodbt@outlook.com
+-Github: https://github.com/Block-T
+-Gitee: https://gitee.com/coodbt
+-版权所有者: Coodbt/Codbt WFC
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let toggleState = false;
+
 function toggleInvertColors() {
     if (!toggleState) {
         document.documentElement.style.filter = 'invert(1)';
@@ -33,3 +34,61 @@ const playOnInteraction = async () => {
     }
 };
 tryPlay();
+
+(function(global) {
+    const NoBackLibrary = {
+        retryCount: -1,
+        successCallback: null,
+        errorCallback: null,
+
+        init: function(options = {}) {
+            this.retryCount = options.retries ?? -1;
+            this.successCallback = options.successCallback ?? null;
+            this.errorCallback = options.errorCallback ?? null;
+            this._setHistory();
+        },
+
+        _setHistory: function() {
+            try {
+                history.pushState(null, null, document.URL);
+                window.addEventListener('popstate', this._handlePopState.bind(this));
+            } catch (e) {
+                if (this.errorCallback) {
+                    this.errorCallback(e);
+                }
+            }
+        },
+
+        _handlePopState: function() {
+            if (this.retryCount === 0) {
+                if (this.errorCallback) {
+                    this.errorCallback();
+                }
+                return;
+            }
+            if (this.retryCount > 0) {
+                this.retryCount--;
+            }
+            history.pushState(null, null, document.URL);
+            if (this.successCallback) {
+                this.successCallback();
+            }
+        },
+    };
+
+    // 导出为全局对象
+    global.NoBackLibrary = NoBackLibrary;
+
+})(window);
+
+NoBackLibrary.init({
+    retries: 2,
+    successCallback: function() {
+        console.log('劝流用户不要离开');
+    },
+    errorCallback: function(error) {
+        alert('你确定要离开吗？或许你还可以多看看。');
+    }
+});
+
+// 写上JS统计代码
