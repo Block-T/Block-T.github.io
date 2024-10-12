873a034019f1b44c04293469a3a26b5b0799e49a
Block-T <coodbt@outlook.com>
2024-10-12 20:55:32 +0800

    A

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Block-T.github.io
 -在线游玩《我的世界Minecraft》，支持JAVA在线网页版和JavaScript在线网页版，并提供其他版本的合集，免费体验MinecraftWeb的所有网页版本。
-
+  
 # 作者信息:
--邮箱: coodbt@outlook.com
--Github: https://github.com/Block-T
--Gitee: https://gitee.com/coodbt
--版权所有者: Coodbt/Codbt WFC
+-邮箱: coodbt@outlook.com \
+-Github: https://github.com/Block-T \
+-Gitee: https://gitee.com/coodbt \
+-作者: Coodbt/Codbt WFC
+  
+# 贡献帮助:
+##-Wzh: https://github.com/wzh656 \
+-使用了该项目主页的部分源码和Css源码，感谢该作者对我们的支持👍👍 \
+##-Wzh的作品: \
+-在线我的世界(原创): https://github.com/wzh656/MinecraftWeb \
+-在线参观: https://wzh656.github.io/MinecraftWeb#DeskGood
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
+/* 点击按钮实现颜色反转 */
 let toggleState = false;
-
 function toggleInvertColors() {
     if (!toggleState) {
         document.documentElement.style.filter = 'invert(1)';
@@ -9,13 +9,11 @@ function toggleInvertColors() {
     }
 }
 
-window.onload = function() {
-    var element = document.getElementById('MinecraftOnline');
-    if (element) {
-        element.parentNode.removeChild(element)
-    }
-};
+/* 删除MinecraftOnline元素 */
+var element = document.getElementById('MinecraftOnline');
+element.parentNode.removeChild(element);
 
+/* JS非自动测试播放功能 */
 const audio = document.getElementById('audio');
 const tryPlay = async () => {
     try {
@@ -34,61 +32,3 @@ const playOnInteraction = async () => {
     }
 };
 tryPlay();
-
-(function(global) {
-    const NoBackLibrary = {
-        retryCount: -1,
-        successCallback: null,
-        errorCallback: null,
-
-        init: function(options = {}) {
-            this.retryCount = options.retries ?? -1;
-            this.successCallback = options.successCallback ?? null;
-            this.errorCallback = options.errorCallback ?? null;
-            this._setHistory();
-        },
-
-        _setHistory: function() {
-            try {
-                history.pushState(null, null, document.URL);
-                window.addEventListener('popstate', this._handlePopState.bind(this));
-            } catch (e) {
-                if (this.errorCallback) {
-                    this.errorCallback(e);
-                }
-            }
-        },
-
-        _handlePopState: function() {
-            if (this.retryCount === 0) {
-                if (this.errorCallback) {
-                    this.errorCallback();
-                }
-                return;
-            }
-            if (this.retryCount > 0) {
-                this.retryCount--;
-            }
-            history.pushState(null, null, document.URL);
-            if (this.successCallback) {
-                this.successCallback();
-            }
-        },
-    };
-
-    // 导出为全局对象
-    global.NoBackLibrary = NoBackLibrary;
-
-})(window);
-
-NoBackLibrary.init({
-    retries: 2,
-    successCallback: function() {
-        console.log('劝流用户不要离开');
-    },
-    errorCallback: function(error) {
-        alert('你确定要离开吗？或许你还可以多看看。');
-    }
-});
-
-// 写上JS统计代码
